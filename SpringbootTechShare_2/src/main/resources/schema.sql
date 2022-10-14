3fe153d7f79acf1d1b9f086200d9f47004ef395c
Tsung-Yuan,Hsieh <dennesshen@gmail.com>
2022-10-14 13:41:55 +0800

    u

--- SpringbootTechShare_2/src/main/resources/schema.sql
+++ SpringbootTechShare_2/src/main/resources/schema.sql
@@ -1,8 +1,13 @@
+CREATE TABLE DEPARTMENT(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+	department_name varchar(50) UNIQUE NOT NULL
+);
+
 CREATE TABLE EMPLOYEE(
-    id INT AUTO_INCREMENT  PRIMARY KEY,
+    id INT AUTO_INCREMENT PRIMARY KEY,
 	employee_name varchar(50) NOT NULL, 
 	employee_position varchar(50),
-	employee_salary int 
+	salary INT
 );
 
 
@@ -12,7 +17,7 @@ CREATE TABLE INVESTBANK(
 );
 
 
-CREATE TABLE FUNDMANGER(
+CREATE TABLE FUNDMANAGER(
     id INT AUTO_INCREMENT PRIMARY KEY,
 	manager_name VARCHAR(50) NOT NULL, 
 	balance INT,
@@ -25,6 +30,6 @@ CREATE TABLE PERSONINFO(
 	idNumber varchar(50) UNIQUE NOT NULL,
 	age INT NOT NULL,
 	address varchar(50),
-	fundmanger_id INT UNIQUE Not null, 
-	FOREIGN KEY(fundmanger_id) REFERENCES FUNDMANGER(id)
+	fundmanager_id INT UNIQUE Not null, 
+	FOREIGN KEY(fundmanager_id) REFERENCES FUNDMANAGER(id)
 );
